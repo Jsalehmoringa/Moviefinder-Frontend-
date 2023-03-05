a3517ad93fcb81173bd4411e035a7784b82d6ccf
jamil <jamil.saleh@student.moringaschool.com>
2023-03-06 02:10:44 +0300

    commit

--- src/components/Movies/Dashboard.js
+++ src/components/Movies/Dashboard.js
@@ -1,23 +1,4 @@
-import React, { useState } from "react";
-
-function Dashboard() {
-
-    const [movies, setMovies] = useState("")
-
-    fetch()
-    .then((response) => response.json())
-    .then((data) => setMovies(data))
-    return(
-        <div>
-            {movies.map((movie) => 
-                <div key={movie.id} className="card">
-                    <h1>{movie.title}</h1>
-                    <p>{movie.description}</p>
-                    <h2>{movie.year}</h2>
-                </div>
-            )}
-        </div>
-    )
-}
-
-export default Dashboard
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Search from "./Search";
+import Allmovies from "./Allmovies";
