942d77dabdf50b25b0342103995557d46f11fb6b
jamil <jamil.saleh@student.moringaschool.com>
2023-03-06 02:11:34 +0300

    commit

--- src/components/Movies/Dashboard.js
+++ src/components/Movies/Dashboard.js
@@ -2,3 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Search from "./Search";
 import Allmovies from "./Allmovies";
+
+
+function Dashboard() {
+
+    const navigate = useNavigate()
+    const [movies, setMovies] = useState([])
+
+  function handleDeleteMovie(id) {
+    const updatedMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(updatedMovies);
+  }
