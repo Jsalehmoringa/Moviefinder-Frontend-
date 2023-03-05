35a13e55e6fbfb53bfb86e5bccd8b69f8b22db51
jamil <jamil.saleh@student.moringaschool.com>
2023-03-06 02:08:37 +0300

    commit

--- src/components/Movies/Allmovies.js
+++ src/components/Movies/Allmovies.js
@@ -1,2 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+function Allmovies({ movies, handleDeleteMovie }) {
+
+    function handleDelete(id) {
+        fetch(`http://127.0.0.1:9292/movies/destroy/${id}`, { 
+            method: "DELETE",
+        })
+
+        handleDeleteMovie(id)
+      }
+ 
+    return(
+        <div className="cards">
+            {movies.map((movie) => 
+                <div key={movie.id} className="card">
+                    <h1>{movie.title}</h1>
+                    <p>{movie.description}</p>
+                    <img className="card-img-top" src={movie.movie_url} alt={movie.movie_url}></img>
+                    <h2>{movie.year}</h2>
+                    {movie.originally_fetched === false && (
+                        <button onClick={() => handleDelete(movie.id)}>Delete</button>
+                    )}
+                    {movie.originally_fetched === false && (
+                        <Link to={`/update/${movie.id}`}>Update</Link>
+                    )}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Allmovies;
