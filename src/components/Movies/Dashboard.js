7a116ee90a155f273c3a0b63b6d4ebf855da9cc8
jamil <jamil.saleh@student.moringaschool.com>
2023-03-06 02:12:17 +0300

    commit

--- src/components/Movies/Dashboard.js
+++ src/components/Movies/Dashboard.js
@@ -13,3 +13,33 @@ function Dashboard() {
     const updatedMovies = movies.filter((movie) => movie.id !== id);
     setMovies(updatedMovies);
   }
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:9292/movies")
+    .then((response) => response.json())
+    .then((data) => setMovies(data))
+    }, [])
+
+    function handleSearch(query) {
+    fetch (`http://127.0.0.1:9292/search?query=${query}`) 
+    .then((response) => response.json())
+    .then((results) => {
+      setMovies(results)
+      navigate("/dashboard")
+    })
+    }
+
+    
+
+    return(
+        <div>
+        <nav className="dashnav">
+            <Link to="/addmovie">Add Movie</Link>
+            <Search onSearch={handleSearch}/>
+        </nav>
+          <Allmovies movies={movies} handleDeleteMovie={handleDeleteMovie}/>
+        </div>
+    )
+}
+
+export default Dashboard
